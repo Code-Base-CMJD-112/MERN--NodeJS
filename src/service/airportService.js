082fa47dbef4ff3f3e46a5b17f3a7619138c4788
ThanuraSilva <thanurasilva1989@gmail.com>
2026-03-21 09:39:12 +0530

    Initiate service - Airports

---
+++ src/service/airportService.js
@@ -0,0 +1,22 @@
+
+
+async function getAllAirports(){
+  //to connnect with Dao / DB
+}
+
+async function saveAirport(){
+
+}
+
+
+async function updateAirport(){
+
+}
+
+
+async function deleteAirport(){
+
+}
+
+
+
