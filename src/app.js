26438a9ba1d134e7798998295a1d7776f16bc645
ThanuraSilva <thanurasilva1989@gmail.com>
2026-03-21 11:55:54 +0530

    Create env variables

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express")
+require("dotenv").config()
 const app = express()
-const PORT = 3500
+const PORT = process.env.PORT || 3500
 const airportRoutes = require("./routes/airportRoutes")
 const mongoose = require("mongoose")
 
