bc5792ef40c1444869bdc73a099354499610af5a
ThanuraSilva <thanurasilva1989@gmail.com>
2026-03-21 10:45:16 +0530

    Create airportService with schema integration

--- src/service/airportService.js
+++ src/service/airportService.js
@@ -1,22 +1,24 @@
 
+const airport = require("../model/airportModel")
 
 async function getAllAirports(){
-  //to connnect with Dao / DB
+  return airport.find()
 }
 
-async function saveAirport(){
-
-}
-
-
-async function updateAirport(){
-
+async function saveAirport(airport){
+   const savedAirport = new airport(airport)
+   return savedAirport.save()
 }
 
 
-async function deleteAirport(){
-
+async function updateAirport(airportId, aiport){
+   return airport.findOneAndUpdate({airportId: airportId},airport,{new: true})
 }
 
 
+async function deleteAirport(airportId){
+   return airport.findOneAndDelete({airportId})
+}
+
+module.exports = { getAllAirports, saveAirport,updateAirport, deleteAirport}
 
