88b6dc5652d67fcfd3f6fee62091fba7b61cf035
ThanuraSilva <thanurasilva1989@gmail.com>
2026-03-21 12:19:16 +0530

    Create authRouter

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -5,4 +5,43 @@ const { addUser } = require("../service/authService")
 const User = require("../model/userModel")
 const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
+const jwtSecret = process.env.JWT_SECRET;
+
+//sign-up
+
+router.post(`${authUrl}/signup`, async(req,res)=>{
+    const {firstName, lastName, email, password,role, phone } = req.body
+
+    if(!firstName || !lastName || !email || !password || !role || !phone){
+       return res.status(401).json({errr: "Missing rquired field/s"})
+    }
+    try{
+        const user = addUser(req.body)
+        //token generate
+        const token = jwt.sign({userId: user.email}, jwtSecret, {expiresIn: '1h'})
+        return res.status(201).json({"message":"User create",token})
+    }catch(err){
+        console.error(err)
+        return res.status(500).json({"message":"Internal Server error"})
+    }
+})
+
+//sign in
+router.post(`${authUrl}/signin`,async (req,res)=>{
+   const { email, password } = req.body;
+
+   //find the user exist
+   const user = await User.findOne({email})
+   const isPasswordValid = await bcrypt.compare(password, user.password)
+
+   if( !user || !isPasswordValid){
+    return res.status(401).json({errr: "Invalid credentils"})
+   }
+   const token = jwt.sign({userId: user.email}, jwtSecret, {expiresIn: '1h'})
+   res.json({token})
+
+})
+
+module.exports  = router
+
 
