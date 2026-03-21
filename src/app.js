497430224a6d753d427461674699b480f2dd7c5c
ThanuraSilva <thanurasilva1989@gmail.com>
2026-03-21 10:14:23 +0530

    Connect MongoDB via mongoose

--- src/app.js
+++ src/app.js
@@ -2,11 +2,22 @@ const express = require("express")
 const app = express()
 const PORT = 3500
 const airportRoutes = require("./routes/airportRoutes")
+const mongoose = require("mongoose")
+
 
 app.use("/airticketer/api/v1",airportRoutes)
 
 app.get("/",(req,res)=>{
    res.send("Hello CMJD-112")
+})
+
+mongoose.connect("mongodb://localhost:27017/airTicketerCmjd112")
+.then(()=>{
+    console.log("MongoDB Connected")
+
+})
+.catch((err)=>{
+    console.error("Failed to connect MongoDB",err)
 })
 
 app.listen(PORT, ()=>{
